83bedd0ca99d49391675c0a2242ca2662484a532
Pixelastic <tim@pixelastic.com>
2015-05-15 17:27:15 +0200

    Adding CSS styling.

--- src/components/Facets/BelongsToMenu/index.js
+++ src/components/Facets/BelongsToMenu/index.js
@@ -23,26 +23,51 @@ var BelongsToMenu = React.createClass( {
     // several BelongsToMenu on the same page.
     return "algolia-belongstomenu";
   },
+  // TODO: This helps in writing BEM classes, but if its usage is to be common
+  // all over the plugin, this should go elsewhere.
+  bemHelper: function(block) {
+    return function(element, modifier) {
+      if (!element) {
+        return block;
+      }
+      if (!modifier) {
+        return block + "--" + element;
+      }
+      return block + "--" + element + "__" + modifier;
+    };
+  },
   render : function() {
     var categories = this.state.categories;
     var uniqueId = this.state.uniqueId;
     var self = this;
+    var bem = this.bemHelper('algolia-belongs-to-menu');
 
     var lis = map( categories, function( count, category ) {
       var inputId = uniqueId + "-" + category;
       var onChange = self.onChange.bind(self, category);
-      return <li key={inputId}>
-        <input type="radio" name={uniqueId} value={category} id={inputId} onChange={onChange} />
-        <label htmlFor={inputId}>
-          {category}
-          <span className="count">{count}</span>
-        </label>
-      </li>;
+      var className = bem('item');
+      if (category === self.state.selectedCategory) {
+        className += " " + bem('item', 'active');
+      }
+
+      return (
+        <li className={className} key={inputId}>
+          <input className={bem('hidden-radio')} type="radio" name={uniqueId} value={category} id={inputId} onChange={onChange} />
+          <label className={bem('label')} htmlFor={inputId} >
+            {category}
+            <span className={bem('count')}>{count}</span>
+          </label>
+        </li>
+      );
     } );
 
-    return <div className="search-filters"><ul>{lis}</ul></div>;
+    return <ul className="algolia-belongs-to-menu">{lis}</ul>;
   },
   onChange : function onChange( category ) {
+    this.setState({ 
+      selectedCategory: category
+    });
+
     this.props.helper.clearRefinements(this.state.facetName);
     if (category !== "All") {
       this.props.helper.toggleRefine( this.state.facetName, category );
