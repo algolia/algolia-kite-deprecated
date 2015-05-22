2a89da5dcb0515828a2388860cb9bd139dbbee4f
Pixelastic <tim@pixelastic.com>
2015-05-22 15:14:04 +0200

    Improve rendering of the blog demo.

--- src/setup/index.js
+++ src/setup/index.js
@@ -98,7 +98,8 @@ module.exports = {
 
       return {
         node : d,
-        placeholder : d.dataset.placeholder
+        placeholder : d.dataset.placeholder,
+        inputClass : d.dataset.inputClass
       };
     } )( dom.querySelector( ".algolia-magic.search-box" ) );
 
