f8cfa3868409463215c1ea17bf52c9da678e2dc8
Alexandre Stanislawski <alexandre.stanislawski@algolia.com>
2015-05-22 13:06:35 +0200

    Let the links handle the linking, and refactoring links

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -3,20 +3,7 @@ var React = require( "react" );
 var forEach = require( "lodash/collection/forEach" );
 
 var Paginator = require( "./Paginator" );
-
-class PaginationLink extends React.Component {
-  render() {
-    var label = this.props.label;
-    var ariaLabel = this.props.ariaLabel;
-    var href = this.props.href;
-    return  <a href="#" aria-label={ariaLabel} onClick={ this.click }>
-              { label }
-            </a>;
-  }
-  click( e ){
-    e.preventDefault();
-  }
-}
+var PaginationLink = require( "./PaginationLink" );
 
 class Pagination extends React.Component {
   render() {
@@ -42,59 +29,46 @@ class Pagination extends React.Component {
   }
   previousPageLink( pager ) {
     return pager.isFirstPage() ?
-      <li className="disabled">
-        <PaginationLink href="#" label={ this.props.labels.previous } ariaLabel="Previous" />
-      </li> :
-      <li onClick={ this.setPage.bind( this, pager.currentPage - 1 ) }>
-        <PaginationLink href="#" label={ this.props.labels.previous } ariaLabel="Previous" />
-      </li>;
+      <PaginationLink href="#" label={ this.props.labels.previous } ariaLabel="Previous"
+                      helper={ this.props.helper } /> :
+      <PaginationLink href="#" label={ this.props.labels.previous } ariaLabel="Previous"
+                      helper={ this.props.helper } page={pager.currentPage - 1}/>;
   }
   nextPageLink( pager ) {
     return pager.isLastPage() ?
-      <li className="disabled">
-        <PaginationLink href="#" label={ this.props.labels.next } ariaLabel="Next" />
-      </li> :
-      <li onClick={ this.setPage.bind( this, pager.currentPage + 1 ) }>
-        <PaginationLink href="#" label={ this.props.labels.next } ariaLabel="Next" />
-      </li>;
+      <PaginationLink href="#" label={ this.props.labels.next } ariaLabel="Next"
+                      helper={ this.props.helper } /> :
+      <PaginationLink href="#" label={ this.props.labels.next } ariaLabel="Next"
+                      helper={ this.props.helper } page={ pager.currentPage + 1 } />
   }
   firstPageLink( pager ) {
     return pager.isFirstPage() ?
-      <li className="disabled">
-        <PaginationLink href="#" label={ this.props.labels.first } ariaLabel="First" />
-      </li> :
-      <li onClick={ this.setPage.bind( this, 0 ) }>
-        <PaginationLink href="#" label={ this.props.labels.first } ariaLabel="First" />
-      </li>;
+        <PaginationLink href="#" label={ this.props.labels.first } ariaLabel="First"
+                        helper={ this.props.helper } /> :
+        <PaginationLink href="#" label={ this.props.labels.first } ariaLabel="First"
+                        helper={ this.props.helper } page={ 0 } />;
   }
   lastPageLink( pager ) {
     return pager.isLastPage() ?
-      <li className="disabled">
-        <PaginationLink href="#" label={ this.props.labels.last } ariaLabel="Last" />
-      </li> :
-      <li onClick={ this.setPage.bind( this, pager.total - 1 ) }>
-        <PaginationLink href="#" label={ this.props.labels.last } ariaLabel="Last" />
-      </li>;
+        <PaginationLink href="#" label={ this.props.labels.last } ariaLabel="Last" /> :
+        <PaginationLink href="#" label={ this.props.labels.last } ariaLabel="Last"
+                        helper={ this.props.helper } page={ pager.total - 1 } />;
   }
   pages( pager ) {
     var pages = pager.pages();
     var elements = [];
 
     forEach( pages, function( pageNumber ) {
-      elements.push( this.page( pager, pageNumber ) );
+      var className = pageNumber === pager.currentPage ? "active" : "";
+      elements.push( 
+        <PaginationLink href="#" label={ pageNumber + 1 } ariaLabel={ pageNumber + 1 }
+                        helper={ this.props.helper } page={ pageNumber }
+                        key={ pageNumber }
+                        className={ className} />
+      );
     }, this );
 
     return elements;
-  }
-  page( pager, pageNumber ) {
-    var className = pageNumber === pager.currentPage ? "active" : "";
-    return <li key={ pageNumber } onClick={ this.setPage.bind( this, pageNumber ) }
-               className={ className } >
-             <PaginationLink href="#" label={ pageNumber + 1 } ariaLabel={ pageNumber + 1 } />
-           </li>;
-  }
-  setPage( newPage ) {
-    this.props.helper.setCurrentPage( newPage ).search();
   }
 }
 
